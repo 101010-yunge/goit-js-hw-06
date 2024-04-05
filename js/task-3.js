5dc9712929bedc49ee92738524ba8804e6010d2d
101010-yunge <720kvm@gmail.com>
2024-04-05 18:17:12 +0200

    add3

--- js/task-3.js
+++ js/task-3.js
@@ -1,31 +1,33 @@
 class StringBuilder {
-    constructor(initialValue) {
-      this._value = initialValue;
-    }
-  
-    getValue() {
-      return this._value;
-    }
-  
-    padEnd(str) {
-      this._value = this._value + str;
-    }
-  
-    padStart(str) {
-      this._value = str + this._value;
-    }
-  
-    padBoth(str) {
-      this.padStart(str);
-      this.padEnd(str);
-    }
+  #value;
+
+  constructor(initialValue) {
+    this.#value = initialValue;
   }
-  
-  const builder = new StringBuilder('.');
-  console.log(builder.getValue()); // "."
-  builder.padStart('^');
-  console.log(builder.getValue()); // "^."
-  builder.padEnd('^');
-  console.log(builder.getValue()); // "^.^"
-  builder.padBoth('=');
-  console.log(builder.getValue()); // "=^.^="
+
+  getValue() {
+    return this.#value;
+  }
+
+  padStart(str) {
+    this.#value = str + this.#value;
+  }
+
+  padEnd(str) {
+    this.#value += str;
+  }
+
+  padBoth(str) {
+    this.padStart(str);
+    this.padEnd(str);
+  }
+}
+
+const builder = new StringBuilder(".");
+console.log(builder.getValue()); // "."
+builder.padStart("^");
+console.log(builder.getValue()); // "^."
+builder.padEnd("^");
+console.log(builder.getValue()); // "^.^"
+builder.padBoth("=");
+console.log(builder.getValue()); // "=^.^="
